937e688fdf21ce1556fbfbed93f406db3f51b79b
hgs6424 <hgs6424@naver.com>
2023-01-06 16:33:11 +0900

    mondoRepository -> reactiveMongoRepository로 수정

--- src/main/kotlin/com/koc/common/config/MongoConfig.kt
+++ src/main/kotlin/com/koc/common/config/MongoConfig.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
 @EnableMongoAuditing
---
+++ src/main/kotlin/com/koc/gateway/adapter/out/persistence/user/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.koc.gateway.adapter.out.persistence.user
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+
+interface UserRepository: ReactiveMongoRepository<UserDocument, String> {
+    suspend fun findByEmailAndPassword(email: String, password: String): UserDocument?
+    suspend fun findByEmail(email: String): UserDocument?
+}
